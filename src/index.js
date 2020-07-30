2896fea3867c00b423770844a6acf78eda4cdd15
Abhisek Kumar <42487773+abhiseksinha23@users.noreply.github.com>
2020-07-30 23:49:28 +0530

    first react app

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Hello from './Hello';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello greeting={"HI, Passing the first agrument!"}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
